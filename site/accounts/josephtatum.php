42d7343b4340ed2cf9ddb5b2dfba84b55275bd9f
Joseph Tatum <joseph@formstack.com>
2016-03-13 14:11:14 -0700

    Attempt Number 2

---
+++ site/accounts/josephtatum.php
@@ -0,0 +1,12 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: josephtatum
+email: josephtatum@gmail.com
+password: >
+  $2a$10$jau6Qs6ZvLjfx.yDDRLoE.c3SxK4TWb2pdaqzqWLS3ovfXsIvKuOe
+language: en
+role: admin
+history:
+  - home
+firstname: Joseph
+lastname: Tatum
