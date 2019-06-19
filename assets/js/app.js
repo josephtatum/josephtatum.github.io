6125f3bb2729c2c940691c1de11b36586c65bd07
Joseph Tatum <josephtatum@gmail.com>
2019-06-19 15:54:05 -0700

    Removal of Last.fm
    removed and disabled last.fm js and html

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@ $(document).foundation();
 
 // LAST.FM
 
-$.getJSON('http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=josephtatum&api_key=e05ea862eda26e264130aa4f95c47c2b&format=json', function(data) {
-  var track = data.recenttracks.track[0]
-  $('#lastfm').html(track.name + " by " + track.artist["#text"])
-});
+// $.getJSON('http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=josephtatum&api_key=e05ea862eda26e264130aa4f95c47c2b&format=json', function(data) {
+//   var track = data.recenttracks.track[0]
+//   $('#lastfm').html(track.name + " by " + track.artist["#text"])
+// });
